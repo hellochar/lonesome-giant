878056c6fccbe52aadb97b50b9af34ea971bcce1
hellochar <hellocharlien@hotmail.com>
2012-09-22 18:15:31 -0700

    yeap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LonesomeGiant::Application.config.secret_token = '245d827b80c823d1f0a144210940e6a3ecc760ec646c4edeb04053cc8991782d477cc644b44bbb6e624f871ba8e7f6afc4fc9bd4d372da8f2d182043d926d61f'
